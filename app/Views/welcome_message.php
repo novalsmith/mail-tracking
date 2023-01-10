e57a3cff973b370c5bc09d82227001f8e9a27b54
novalsmith <novalsmith69@gmail.com>
2023-01-10 18:24:31 +0700

    update

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -12,7 +12,7 @@
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')?>">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css")?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css")?>">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/vendor/animate/animate.css")?>">
 <!--===============================================================================================-->	
@@ -78,21 +78,21 @@
 	</div>
 	
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("assets/vendor/jquery/jquery-3.2.1.min.js")?>></script>
+	<script src="<?php echo base_url("assets/vendor/jquery/jquery-3.2.1.min.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("assets/vendor/animsition/js/animsition.min.js")?>></script>
+	<script src="<?php echo base_url("assets/vendor/animsition/js/animsition.min.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("assets/vendor/bootstrap/js/popper.js")?>></script>
-	<script src="<?php echo base_url("assets/vendor/bootstrap/js/bootstrap.min.js")?>></script>
+	<script src="<?php echo base_url("assets/vendor/bootstrap/js/popper.js")?>"></script>
+	<script src="<?php echo base_url("assets/vendor/bootstrap/js/bootstrap.min.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("assets/vendor/select2/select2.min.js")?>></script>
+	<script src="<?php echo base_url("assets/vendor/select2/select2.min.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("assets/vendor/daterangepicker/moment.min.js")?>></script>
-	<script src="<?php echo base_url("assets/vendor/daterangepicker/daterangepicker.js")?>></script>
+	<script src="<?php echo base_url("assets/vendor/daterangepicker/moment.min.js")?>"></script>
+	<script src="<?php echo base_url("assets/vendor/daterangepicker/daterangepicker.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("assets/vendor/countdowntime/countdowntime.js")?>></script>
+	<script src="<?php echo base_url("assets/vendor/countdowntime/countdowntime.js")?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url("assets/js/main.js")?>></script>
+	<script src="<?php echo base_url("assets/js/main.js")?>"></script>
 
 </body>
 </html>
