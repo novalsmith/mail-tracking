695021288bebdfd3958230915e8b7fbf613099a1
novalsmith <novalsmith69@gmail.com>
2023-01-15 20:18:43 +0700

    changes configurations

--- service/public/index.php
+++ service/public/index.php
@@ -29,7 +29,8 @@ chdir(FCPATH);
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
+// dev_core its mean core folder in online service
+require FCPATH . '../dev_core/service/app/Config/Paths.php';
 // ^^^ Change this line if you move your application folder
 
 $paths = new Config\Paths();
